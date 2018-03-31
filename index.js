86103ddbb98e89991dae9f6cde9172001369bdb7
Huynh Tuan Anh <huynhtuananh21895@gmail.com>
2018-03-31 20:24:02 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-var path = require('path');
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
 
-app.set('port', (process.env.PORT || 5000));
+const app = express();
 
-app.use(express.static(__dirname + '/public'));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'public/index.html'));
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+const port = process.env.PORT || 5000;
+app.listen(port);
 
+console.log(`Password generator listening on ${port}`);
